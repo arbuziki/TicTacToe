2745e4c444bbe253e2444f25d5681ef07d33e91f
arbuziki <parfenovee@mail.ru>
2016-07-22 20:16:39 +0300

    New Library for Bots

---
+++ Bots/EgorBot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.Game.Rules;
+
+namespace Bots
+{
+    public class EgorBot : IPlayer
+    {
+        public string Name { get; }
+        public StepType StepType { get; }
+        public void BeginGame(IGame game)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IStep MakeStep()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
